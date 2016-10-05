d47fbacbb445ea3def704c9028a90b6f5d70771c
suresh kumar <1755463@student.swin.edu.au>
2016-10-05 12:07:57 +1100

    added win or lose display after after game end for each level

--- C#/battleships/src/EndingGameController.cs
+++ C#/battleships/src/EndingGameController.cs
@@ -25,8 +25,10 @@ static class EndingGameController
 
 		if (GameController.HumanPlayer.IsDestroyed) {
 			SwinGame.DrawTextLines("YOU LOSE!", Color.White, Color.Transparent, GameResources.GameFont("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth(), SwinGame.ScreenHeight());
-		} else {
+		} 
+		else if(GameController.ComputerPlayer.IsDestroyed ){
 			SwinGame.DrawTextLines("-- WINNER --", Color.White, Color.Transparent, GameResources.GameFont("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth(), SwinGame.ScreenHeight());
+		
 		}
 	}
 
